3e8a1e6a90f96b32e0291f10841d98f314602998
Alexis LEDOUX <alexis.ledoux@edu.devinci.fr>
2020-12-15 18:29:55 +0100

    first recuperation done

--- ProjectFile.py
+++ ProjectFile.py
@@ -1,26 +1,36 @@
 # coding: utf-8
 import imaplib
 import email
-imap = imaplib.IMAP4_SSL('imap.gmail.com')
-imap.login('projetiosnetwork@gmail.com','projectisfun')
-(status, res) = imap.list()
-# renvoie ('OK', ['nombre de messages']) ou sinon ('NO', ['message erreur'])
-(status, numberMessages) = imap.select('INBOX')
-print(status, 'Nombre de messages = ', numberMessages)
-# renvoie par exemple ('OK', ['1 2 3 4 5']) qui sont les numéros des messages.
-(status, searchRes) = imap.search(None, 'ALL')
-# Récupération des numéros des messages
-ids = searchRes[0].split()
-for i in range(len(ids)):
-    # Récupère seulement l'expéditeur et le sujet dans le header
-    (status, res) = imap.fetch(ids[i], '(BODY[HEADER.FIELDS (FROM SUBJECT)])')
-    for responsePart in res:
-        if isinstance(responsePart, tuple):
-            response = responsePart[1]
-            msg = email.message_from_bytes(response)
-            sender = msg['from'].encode("ASCII").decode("utf-8")
-            subject = msg['subject'].encode("ASCII").decode("utf-8")
-            print('expediteur : ', sender)
-            print('sujet : ', subject)
-imap.close()
-imap.logout()
+
+
+def recup_datagmail(mail:str,password:str):
+    imap = imaplib.IMAP4_SSL('imap.gmail.com')
+    imap.login(mail,password)
+    (status, res) = imap.list()
+    # renvoie ('OK', ['nombre de messages']) ou sinon ('NO', ['message erreur'])
+    (status, numberMessages) = imap.select('INBOX')
+    print(status, 'Nombre de messages = ', numberMessages)
+    # renvoie par exemple ('OK', ['1 2 3 4 5']) qui sont les numéros des messages.
+    (status, searchRes) = imap.uid("search",None, 'ALL')
+    # Récupération des numéros des messages
+    ids = searchRes[0].split()
+    for i in range(len(ids)):
+        # Récupère seulement l'expéditeur et le sujet dans le header
+        (status, res) = imap.fetch(ids[i], '(BODY[HEADER.FIELDS (FROM SUBJECT TO DATE)])')
+        for responsePart in res:
+            if isinstance(responsePart, tuple):
+                msg = email.message_from_bytes(responsePart[1])
+                sender = str(email.header.make_header(email.header.decode_header(msg['From'])))
+                subject = str(email.header.make_header(email.header.decode_header(msg['Subject'])))
+                receiver = str(email.header.make_header(email.header.decode_header(msg['To'])))
+                date = str(email.header.make_header(email.header.decode_header(msg['Date'])))
+                print(receiver)
+                print(date)
+                print('expediteur : ', sender)
+                print('sujet : ', subject)
+    imap.close()
+    imap.logout()
+
+
+if __name__ == "__main__":
+    recup_datagmail('projetiosnetwork@gmail.com','projectisfun')
